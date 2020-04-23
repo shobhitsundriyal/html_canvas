7829114abe2fe2ddf4861538f7469537c465d22d
unknown <shobhitsundriyal0170@gmail.com>
2020-04-23 19:29:50 +0530

    getcontext?

---
+++ Draw strokes/main.js
@@ -0,0 +1,13 @@
+var canvas = document.getElementById('mycanvas')
+
+var ctx = canvas.getContext('2d')
+
+ctx.fillRect(100,100,100,100)
+
+ctx.strokeRect(100,250,100,100)
+
+ctx.rect(300,100,100,100)
+ctx.fillStyle = 'blue'
+ctx.fillStyle()
+ctx.strokeStyle = 'red'
+ctx.stroke()
